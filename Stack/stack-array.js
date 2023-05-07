29e3575ed83e88b658c2da99b835d82c98a0b381
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-05-07 09:12:37 +0530

    stack did in array

---
+++ Stack/stack-array.js
@@ -0,0 +1,49 @@
+class Stack {
+    constructor() {
+        this.items = []
+    }
+
+    push(data) {
+        this.items.push(data)
+    }
+
+    isEmpty() {
+        return this.items.length === 0 
+    }
+
+    pop() {
+        if(this.isEmpty()) {
+            return "Underflow"
+        }
+        return this.items.pop()
+    }
+
+    peek() {
+        if(this.isEmpty()) {
+            return "Underflow"
+        }
+        return this.items[this.items.length - 1]
+    }
+
+    display() {
+        for(let i = this.items.length-1; i >= 0; i--) {
+            console.log(this.items[i])
+        }
+    }
+}
+
+const stack = new Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.display()
+console.log('\n')
+
+console.log('Popped: ',stack.pop());
+console.log('\n')
+
+stack.push(4)
+stack.display()
+console.log('\n')
+
+console.log('Peek :',stack.peek());
