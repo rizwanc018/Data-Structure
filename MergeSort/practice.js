ada555f26bd3f8c4748157b82d518f9b71bed0d4
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-05-17 16:46:25 +0530

    Merge Sort

---
+++ MergeSort/practice.js
@@ -0,0 +1,32 @@
+const mergeSort = arr => {
+    if (arr.length <= 1) return arr
+    let m = Math.floor(arr.length / 2)
+    let lArr = arr.slice(0, m)
+    let rArr = arr.slice(m)
+    return merge(mergeSort(lArr), mergeSort(rArr))
+}
+
+const merge = (lArr, rArr) => {
+    let sArr = []
+    let li = 0, ri = 0
+
+    while (li < lArr.length && ri < rArr.length) {
+        if (lArr[li] < rArr[ri]) {
+            sArr.push(lArr[li++])
+        } else {
+            sArr.push(rArr[ri++])
+        }
+    }
+    while (li < lArr.length) {
+        sArr.push(lArr[li++])
+    }
+    while (ri < rArr.length) {
+        sArr.push(rArr[ri++])
+    }
+
+    return sArr
+}
+
+
+let arr = [234, 43, 55, 63, 5, 6, 235, 547, 33]
+console.log(mergeSort(arr));
---
+++ QuickSort/practice.js
@@ -0,0 +1,29 @@
+const partition = (arr, s, e) => {
+let pivot = arr[Math.floor(s + (e - s) / 2)]
+    while (s <= e) {
+        while (arr[s] < pivot) {
+            s++
+        }
+        while (arr[e] > pivot) {
+            e--
+        }
+        if (s <= e) {
+            [arr[s], arr[e]] = [arr[e], arr[s]]
+            s++
+            e--
+        }
+    }
+    return s
+}
+
+const qsa = (arr, l, r) => {
+    if (l < r) {
+        const i = partition(arr, l, r)
+        qsa(arr, l, i - 1)
+        qsa(arr, i, r)
+    }
+    return arr
+}
+
+let arr = [234, 43, 55, 63, 5, 6, 235, 547, 33]
+console.log(qsa(arr, 0, arr.length - 1));
