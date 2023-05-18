43d657fe8308e6c6b81cbf5a623d6a75511a6270
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-05-18 12:38:53 +0530

    insertion practice

--- InsertionSort/practice.js
+++ InsertionSort/practice.js
@@ -11,15 +11,28 @@
 //     return arr
 // }
 
-const insertionSort = (arr) => {
-    for (let i = 1; i < arr.length; i++) {
+// const insertionSort = (arr) => {
+//     for (let i = 1; i < arr.length; i++) {
+//         let curr = arr[i]
+//         let j = i - 1
+//         while (j >= 0 && curr < arr[j]) {
+//             arr[j + 1] = arr[j]
+//             j--
+//         }
+//         arr[j + 1] = curr
+//     }
+//     return arr
+// }
+
+const insertionSort = arr => {
+    for(let i = 1; i < arr.length; i++) {
         let curr = arr[i]
         let j = i - 1
-        while (j >= 0 && curr < arr[j]) {
-            arr[j + 1] = arr[j]
+        while(j >= 0 && curr < arr[j]){
+            arr[j+1] = arr[j]
             j--
         }
-        arr[j + 1] = curr
+        arr[j+1] = curr
     }
     return arr
 }
