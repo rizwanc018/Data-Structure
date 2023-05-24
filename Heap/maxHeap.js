8ee2f81868fb09a9f88cd5d72d150ad143c37505
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-05-24 16:33:56 +0530

    min Heap

--- Heap/maxHeap.js
+++ Heap/maxHeap.js
@@ -4,9 +4,10 @@ class MaxHeap {
     }
 
     heapify(arr) {
+        this.heap = arr
         let l = arr.length
         for (let i = (l / 2) - 1; i >= 0; i--) {
-            this.heapifyHelper(arr, l, i)
+            this.shiftDown(i)
         }
     }
     heapifyHelper(arr, n, i) {
@@ -19,6 +20,33 @@ class MaxHeap {
             [arr[i], arr[largest]] = [arr[largest], arr[i]];
             this.heapifyHelper(arr, n, largest)
         }
+    }
+
+    shiftDown(i) {
+        let l = 2 * i + 1
+        while (l < this.heap.length) {
+            let r = 2 * i + 2
+            let idxtoShift
+            if (r < this.heap.length && this.heap[r] > this.heap[i])
+                idxtoShift = r
+            else idxtoShift = l
+            if (this.heap[idxtoShift] > this.heap[i])
+                [this.heap[i], this.heap[idxtoShift]] = [this.heap[idxtoShift], this.heap[i]];
+            i = idxtoShift
+            l = 2 * i + 1
+        }
+    }
+
+    shiftUp(i) {
+        let p = (i - 1) / 2
+        while (p >= 0 && this.heap[i] > this.heap[p]) {
+            [this.heap[i], this.heap[p]] = [this.heap[p], this.heap[i]];
+            p = (p - 1) / 2
+        }
+    }
+
+    peek() {
+        return this.heap[0]
     }
 }
 
---
+++ Heap/minHeap.js
@@ -0,0 +1,73 @@
+class minHeap {
+    constructor() {
+        this.heap = []
+    }
+
+    heapify(arr) {
+        this.heap = arr
+        let l = arr.length
+        for (let i = l / 2 - 1; i >= 0; i--) {
+            this.shiftDown(i)
+        }
+    }
+
+    // heapifyHelper(arr, n, i) {
+    //     let s = i
+    //     let l = 2 * i + 1
+    //     let r = 2 * i + 2
+    //     if (l < n && arr[l] < arr[s]) s = l
+    //     if (l < n && arr[r] < arr[s]) s = r
+    //     if (s !== i) {
+    //         [arr[s], arr[i]] = [arr[i], arr[s]]
+    //         this.heapifyHelper(arr, n, s)
+    //     }
+    // }
+
+    shiftDown(i) {
+        let l = 2 * i + 1
+        while (l < this.heap.length) {
+            let r = 2 * i + 2
+            let s
+            if (r < this.heap.length && this.heap[r] < this.heap[i]) s = r
+            else s = l
+            if (this.heap[s] < this.heap[i]) {
+                [this.heap[s], this.heap[i]] = [this.heap[i], this.heap[s]]
+                i = s
+                l = 2 * i + 1
+            } else return
+        }
+    }
+    shiftUp(i) {
+        let p = (i - 1) / 2
+        while (p >= 0 && this.heap[i] < this.heap[p]) {
+            [this.heap[i], this.heap[p]] = [this.heap[p], this.heap[i]];
+            i = p
+            p = (p - 1) / 2
+        }
+    }
+    
+    insert(val) {
+        this.heap.push(val)
+        this.shiftUp(this.heap.length - 1)
+    }
+
+    remove() {
+        if (this.heap.length === 0) return
+        if (this.heap.length === 1) {
+            this.heap.pop()
+            return
+        }
+        this.heap[0] = this.heap.pop()
+        this.shiftDown(0)
+    }
+
+}
+
+let a = [1, 5, 15, 7]
+const heap = new minHeap()
+heap.heapify(a)
+console.log(heap.heap)
+heap.remove()
+console.log(heap.heap)
+heap.insert(2)
+console.log(heap.heap)
