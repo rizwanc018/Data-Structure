1a4665c08179ccd44b2ef03d26cf8ac2186007df
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-05-25 10:37:00 +0530

    Graph

---
+++ Graph/practice.js
@@ -0,0 +1,48 @@
+class Graph {
+    constructor() {
+        this.vertexMap = new Map()
+    }
+
+    addVertex(vertex) {
+        if (!this.vertexMap.has(vertex)) {
+            this.vertexMap.set(vertex, [])
+        }
+    }
+
+    addEdge(vertex, edge, biDir = false) {
+        if (!this.vertexMap.has(vertex)) this.addVertex(vertex)
+        if (!this.vertexMap.has(edge)) this.addVertex(edge)
+        this.vertexMap.get(vertex).push(edge)
+        if (biDir) this.vertexMap.get(edge).push(vertex)
+    }
+
+    removeEdge(vertex, edge) {
+        if (this.vertexMap.has(vertex))
+            this.vertexMap.get(vertex).splice(this.vertexMap.get(vertex).indexOf(edge), 1)
+    }
+
+    removeVertex(vertex) {
+        if (this.vertexMap.has(vertex)) {
+            this.vertexMap.delete(vertex)
+            for (let [v, e] of this.vertexMap)
+                this.vertexMap.set(v, e.filter(edge => edge !== vertex))
+        }
+    }
+
+    display() {
+        for (let [vertex, edge] of this.vertexMap)
+            console.log(vertex, ' -> ', edge)
+    }
+}
+
+const graph = new Graph();
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D", true);
+graph.addEdge("C", "D", true);
+graph.addEdge("F", "B", true);
+graph.display();
+console.log()
+// graph.removeVertex('D')
+graph.removeEdge('B', 'F')
+graph.display();
